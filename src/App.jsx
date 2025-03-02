e2b842e36c0aa9d5fdf87498f17bce0b1bd74f2d
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-03-02 22:24:54 +0545

    Component in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+export const App =()=>{
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+    <>
+      <ArunPanda/>
+    <NetflixCards/>
+    <NetflixCards/>
+    <NetflixCards/>
+    </>
+  );
+
+};
+
+
+const NetflixCards =()=>{
+  return (
+    <div >
+    
+    <div >
+      <img src="images/tumbbadposter.webp" alt="images/tumbadposter" />
+    </div>
+    <div >
+      <h1>Tumbbad</h1>
+      <h3>Release Date: 12 October 2018</h3>
+      <h3>
+        Rating: 8.3/10
+      </h3>
+      <p>
+        Tumbbad is a 2018 Indian Hindi-language period horror film directed by
+        Rahi Anil Barve and Adesh Prasad. Anand Gandhi, best known for the
+        National Award-winning film Ship of Theseus, brought it to life as a
+        creative director. 
+      </p>
+    </div>
+  </div>
+  );
+};
+
+
+const ArunPanda = () => {
+  return (
+    <header>
+      <h1>My Favourite Movies</h1>
+    </header>
+  );
+};
+
+
+
+
