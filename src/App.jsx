eef31daebc2d726cb7d6178a3c541df855e0f25a
ArunPandeyLaudari <arpa_bca2079@lict.edu.np>
2025-03-02 23:02:38 +0545

    Dynamic Value  in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,44 @@
 
 
-export const App =()=>{
+export const App = () => {
   return (
 
     <>
-      <ArunPanda/>
-    <NetflixCards/>
-    <NetflixCards/>
-    <NetflixCards/>
+      <ArunPanda />
+      <NetflixCards />
     </>
   );
 
 };
 
 
-const NetflixCards =()=>{
+const NetflixCards = () => {
+  const images = "/images/tumbbadposter.webp";
+  const name = "Tumbbad"
+  const ReleaseDate = "12 October 2018";
+  const Rating = "8.3/10";
+  const Description = "Tumbbad is a 2018 Indian Hindi-language period horror film directed by Rahi Anil Barve and Adesh Prasad. Anand Gandhi, best known for the National Award-winning film Ship of Theseus, brought it to life as a creative director.";
   return (
+
     <div >
-    
-    <div >
-      <img src="images/tumbbadposter.webp" alt="images/tumbadposter" />
+
+      <div >
+        <img src={images} alt="images/tumbadposter" />
+      </div>
+      <div >
+        <h1>Name: {name}</h1>
+        <h3>Release Date: {ReleaseDate}</h3>
+        <h3>
+          Rating: {Rating}
+        </h3>
+        <h3>
+          Description: {Description}
+        </h3>
+        <h3> Genere:
+          {returnGenre()}
+        </h3>
+      </div>
     </div>
-    <div >
-      <h1>Tumbbad</h1>
-      <h3>Release Date: 12 October 2018</h3>
-      <h3>
-        Rating: 8.3/10
-      </h3>
-      <p>
-        Tumbbad is a 2018 Indian Hindi-language period horror film directed by
-        Rahi Anil Barve and Adesh Prasad. Anand Gandhi, best known for the
-        National Award-winning film Ship of Theseus, brought it to life as a
-        creative director. 
-      </p>
-    </div>
-  </div>
   );
 };
 
@@ -42,11 +46,15 @@ const NetflixCards =()=>{
 const ArunPanda = () => {
   return (
     <header>
-      <h1>My Favourite Movies</h1>
+      <h1>My Favourite Movies {1 + 2 + 3}</h1>
     </header>
   );
 };
 
 
+const returnGenre = () => {
+  const genre ="Horror";
+  return genre;
 
+};
 
